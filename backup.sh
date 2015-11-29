91a835c601591fcf66b5b67a3e80e5eeb3df9c09
Stefan Lendl <ste.lendl@gmail.com>
2015-11-29 23:44:08 +0100

    updated backup.sh -r restore, -d dest, -s src, -a add

--- backup.sh
+++ backup.sh
@@ -1,46 +1,110 @@
 #!/bin/bash
 
-[ "$1" == "-h" ] && echo "Usage: $0 destination [addtional flags]" && exit 0
+usage(){
+   echo "Usage: $0 [options]"
+   echo "-h | --help"
+   echo "-s | --src        Source directory which will be backed up (use without trailing /)"
+   echo "-d | --dest       Destination dir where the backup will be stored"
+   echo "-r | --restore    Restore the backupt -s <backup location> -d <restore dest>"
+   echo "-c | --cycle      Update the tail of cycling backups. Not recomendet for snapshots"
+   echo "                  [not yet implemented]"
+   echo "-a | --add        Addition flags passed to rsync like \"-q --dry-run --delete-excluded...\""
+   exit 1
+}
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-if [ $# -lt 1 ]; then
-    echo "No destination defined. Usage: $0 destination [addtional flags]" >&2
-    exit 1
-elif [ $# -gt 2 ]; then
-    echo "Too many arguments. Usage: $0 destination [addtional flags]" >&2
-    exit 1
-elif [ ! -d "$1" ]; then
-   mkdir -p "$1"
-   if [ $? != "0" ]; then
-      echo "Invalid path: $1" >&2
-      exit 1
+SRC="/" # default src for root backup
+
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+       usage
+    ;;
+    -s|--src)
+       SRC="$2"
+       src_set=true
+       shift
+    ;;
+    -d|--dst)
+       DEST="$2"
+       shift
+    ;;
+    -r|--restore)
+       RESTORE=true
+    ;;
+    -c|--cycle)
+       echo "cycling not yet implemented"
+       CYCLE=true
+       shift
+    ;;
+    -a|--add)
+       ADDITION_FLAGS=$2
+       shift
+    ;;
+    *)
+       echo "unknown argument $key"
+       usage
+    ;;
+esac
+shift
+done
+
+if [ $RESTORE ] && ([ ! $src_set ] || [ -z ${DEST+x} ]); then
+   echo "restore mode requires -s and -d" >&2
+   usage
+fi
+
+mkdir -p "$DEST"
+if [ "$?" != "0" ]; then
+   echo "Invalid path: $DEST" >&2
+   exit 1
+fi
+
+if [ ! -d "$SRC" ]; then
+   echo "Invalid source $SRC" >&2
+   exit 1
+elif [ $RESTORE ] && [ ! -e $SRC/backup_from ]; then
+   echo "this is not of my backups - no \$SRC/backup_from found"
+   read -rsp $"continue anyway? (y/N): " -n1 cont
+   if [ "y" != "$cont" ]; then
+      exit
    fi
 fi
 
-case "$1" in
-  "/mnt") ;;
-  "/mnt/"*) ;;
-  "/media") ;;
-  "/media/"*) ;;
-  "/run/media") ;;
-  "/run/media/"*) ;;
-  *) echo "Destination not allowed." >&2
-     exit 1
-     ;;
-esac
-
-if [ ! -z "$2" ]; then
-   # f.e. --quiet -q
-   addition_flags="$2"
+if [ ! $RESTORE ]; then
+   case "$DEST" in
+      "/mnt") ;;
+      "/mnt/"*) ;;
+      "/media") ;;
+      "/media/"*) ;;
+      "/run/media") ;;
+      "/run/media/"*) ;;
+      *) echo "Destination not allowed." >&2
+         exit 1
+         ;;
+   esac
 fi
 
 START=$(date +%s)
-rsync -aAXhH --info=progress2 --delete $addition_flags --exclude={"/home/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","swapfile*"} / "$1"
+rsync -aAXhH --info=progress2 --delete $ADDITION_FLAGS \
+      --exclude={"/home/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","swapfile*"} \
+      $SRC $DEST
 FINISH=$(date +%s)
 
-echo "$(date '+%Y-%m-%d, %T, %A')" > $1/backup_from 
-echo "total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds" >> $1/backup_from 
+if [ ! $RESTORE ]; then
+   touch $DEST # update mtime
+   echo "$(date '+%Y-%m-%d, %T, %A')" > $DEST/backup_from
+   echo "total time: $(( ($FINISH-$START) / 60 )) minutes, $(( ($FINISH-$START) % 60 )) seconds" >> $DEST/backup_from
+else 
+   rm $DEST/backup_from -f
+   # removing the backup_from when restoring
+fi
+
+exit 0
